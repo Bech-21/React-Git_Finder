5e01bbe8433f6db5b74b20598442c93e189829e0
Bech-21 <50781776+Bech-21@users.noreply.github.com>
2021-02-02 23:32:59 -0700

    Search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./styles.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/layout/users/Users";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import axios from "axios";
 
 class App extends Component {
@@ -12,15 +13,20 @@ class App extends Component {
   async componentDidMount() {
     this.setState({ loading: true });
     const res = await axios.get("https://api.github.com/users");
-    console.log(res.data);
+
     this.setState({ users: res.data, loading: false });
   }
+
+  searchUsers = (text) => {
+    console.log(text);
+  };
   render() {
     return (
       <div>
-        j
         <Navbar />
+
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
